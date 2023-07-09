5df5f029e04164b94377ff85b1d96182ee6051c8
Bwhitman33 <bwhitman33@gmail.com>
2023-07-09 11:14:07 -0400

    corrected error in schema and added appStart function to test as I go. Its unsurprisingly not working

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (18,2) NOT NULL,
-    department_id INT NIT NULL, 
+    department_id INT NOT NULL, 
     FOREIGN KEY(department_id)
     REFERENCES department(id)
 );
